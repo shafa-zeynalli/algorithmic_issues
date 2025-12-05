e34c94b2b4d905f5954b0da089117612c1842002
Shafa Zeynalli <shafa.zeynalli@outlook.com>
2025-12-05 20:47:40 +0400

    Add solved stack algorithms

--- src/algoritm/stacks/DailyTemperatures.java
+++ src/algoritm/stacks/DailyTemperatures.java
@@ -9,26 +9,25 @@ public class DailyTemperatures {
         int[] arr = {73,74,75,71,69,72,76,73};
         System.out.println(dailyTemperatures(arr));
     }
-//    public static int[] dailyTemperatures(int[] temperatures) {
-//        Map<Integer,Integer> m= new HashMap<>();
-//        Stack<Integer> stack =new Stack<>();
-//
-//        for (int i=temperatures.length-1; i>=0; i--){
-//            int count=0;
-//            while (!stack.isEmpty() && stack.peek()<=temperatures[i]){
-//                count++;
-//                stack.pop();
-//            }
-//
-//            int grather = stack.isEmpty() ? 0 : count;
-//
-//            m.put(temperatures[i], grather);
-//            stack.push(temperatures[i]);
-//        }
-//
-//        for (int i = 0; i < temperatures.length; i++) {
-//            temperatures[i]=m.get(temperatures[i]);
-//        }
-//        return temperatures;
-//    }
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n= temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stack =new Stack<>();
+
+        for (int i=n-1; i>=0; i--){
+
+            while (!stack.isEmpty() && temperatures[stack.peek()]<=temperatures[i]){
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()){
+                res[i]=stack.peek()-i;
+            }else {
+                res[i]=0;
+            }
+            stack.push(i);
+        }
+
+        return res;
+    }
 }
